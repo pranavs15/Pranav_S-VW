fa39582656864dea871aa5c16796523633b04bbb
pranavs15 <120391339+pranavs15@users.noreply.github.com>
2026-02-13 12:54:11 +0530

    Delete Assignments/04day/Assignment 6 directory

--- Assignments/04day/Assignment 6/q5.py
+++
@@ -1,8 +0,0 @@
-# Question 5:
-# Replace each 'this' with 'That' in a string.
-
-text = input("Enter a string: ")
-
-updated_text = text.replace("this", "That")
-
-print("Updated String:", updated_text)
--- Assignments/04day/Assignment 6/q6.py
+++
@@ -1,9 +0,0 @@
-# Question 6:
-# Double all numbers in a list using map and lambda.
-
-numbers = [1, 2, 3, 4, 5]
-
-doubled = list(map(lambda x: x * 2, numbers))
-
-print("Original List:", numbers)
-print("Doubled List:", doubled)
